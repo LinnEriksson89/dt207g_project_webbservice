60c8b15319f539e7ab4c6d1a90e98ee1003b25f3
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2024-09-05 13:46:04 +0200

    Working version of food-class that handles weekly menus

--- classes/Food.js
+++ classes/Food.js
@@ -99,163 +99,132 @@ class Food {
             //If none of the above is true the code continues and calculates the menu.
         });
         
-        //Get all items from the lunch-category with priority=1.
-        connection.query(`SELECT item_id, name, description, allergens, lunchday, lunchprio FROM lunch_item WHERE lunchprio=1;`, (err, result) => {
+        //Get all items from the lunch-category.
+        connection.query(`SELECT item_id, name, description, allergens, lunchday, lunchprio FROM lunch_item ORDER BY lunchprio, lunchday, item_id;`, (err, result) => {
             //If-else for error handling and result.
             if(err) {
               //Error message, http response code and return.
               return json({status: 500, error: "Something went wrong."});
             } else {
-                //Variable for counting number of lunches.
-                let amount = 0;
-                //Push result to lunches.
-                for (let i = 0; i < result.length; i++) {
-                    lunches.push(result[i]);
-                    //Increase lunchcounter.
-                    amount++;
+                //Loop through results and add all with lunchprio=1 to lunches.
+                result.forEach(item => {
+                    if(item.lunchprio === 1) {
+                        lunches.push(item);
+                    }
+                });
+
+                //Are there enough items? Otherwise we need to push prio=2 too.
+                if(lunches.length < 14) {
+                    result.forEach(item => {
+                        if(item.lunchprio === 2) {
+                            lunches.push(item);
+                        }
+                    });
                 }
 
-                //Are there enough items?
-                if(amount < 14) {
-                    console.log("Test!!");
-                    //If there are fewer than 14 lunches, get lower prio lunches too.
-                    connection.query(`SELECT item_id, name, description, allergens, lunchday, lunchprio FROM lunch_item WHERE lunchprio=2;`, (err, result) => {
-                        //If-else for error handling and result.
-                        if(err) {
-                            //Error message, http response code and return.
-                            return json({status: 500, error: "Something went wrong."});
-                        } else {
-                            //Push result to lunches.
-                            for (let i = 0; i < result.length; i++) {
-                                lunches.push(result[i]);
-                            }
-
-                            //Are there enough items?
-                            if(amount < 14) {
-                                //If there are fewer than 14 lunches, get lower prio lunches too.
-                                connection.query(`SELECT item_id, name, description, allergens, lunchday, lunchprio FROM lunch_item WHERE lunchprio=3;`, (err, result) => {
-                                    //If-else for error handling and result.
-                                    if(err) {
-                                        //Error message, http response code and return.
-                                        return json({status: 500, error: "Something went wrong."});
-                                    } else {
-                                        //Push result to lunches.
-                                        for (let i = 0; i < result.length; i++) {
-                                            lunches.push(result[i]);
-                                        }
-                                    }
-                                });
-                            }
+                //Are there enough items? Otherwise we need to push prio=3 too.
+                if(lunches.length < 14) {
+                    result.forEach(item => {
+                        if(item.lunchprio === 3) {
+                            lunches.push(item);
                         }
                     });
                 }
             }
-            console.log(lunches);
 
-            //Lunches should now be an array with at least 14 item_ids.
-            //Remove items to get exactly 14 id's in array.
-            while(lunches.length > 15) {
+            //Lunches should now be an array with at least 14 items.
+            //Remove items to get exactly 14 items in array.
+            while(lunches.length > 14) {
                 lunches.pop();
             }
 
-            //Create a new array with 14 items.
+            //Create a new array with 14 items and a copy of lunches.
             const weeklymenu = new Array(14);
+            let lunchesCopy = lunches.slice(0);
 
-            //Use a for loop with a if/else-statetments to sort according to lunchdays
-            //This is absolutely not the best way to do this, but it's the way I could figure out.
+            //Use a for loop with if/else-statetments to add to weeklyMenu according to lunchdays.
+            //The idea is that Monday is weeklyMenu[0] and weeklyMenu[1], Tuesday is [2] and [3] and so on.
+            //This is probably not the best way to do this, but I learned alot about slice and splice to get it to work.
             let i = 0;
             lunches.forEach(lunch => {
                 if(lunch.lunchday == 1){
                     if(!weeklymenu[0]) {
-                        weeklymenu[0] = lunches[i];
+                        weeklymenu[0] = lunch;
                     } else if(!weeklymenu[1]) {
-                        weeklymenu [1] = lunches[i];
+                        weeklymenu[1] = lunch;
                     }
-                    lunches.splice(i, 1);
+                    lunchesCopy.splice(i, 1);
+                    i--;
                 } else if(lunch.lunchday == 2){
                     if(!weeklymenu[2]) {
-                        weeklymenu[2] = lunches[i];
+                        weeklymenu[2] = lunch;
                     } else if(!weeklymenu[3]) {
-                        weeklymenu [3] = lunches[i];
+                        weeklymenu[3] = lunch;
                     }
-                    lunches.splice(i, 1);
+                    lunchesCopy.splice(i, 1);
+                    i--;
                 } else if(lunch.lunchday == 3){
                     if(!weeklymenu[4]) {
-                        weeklymenu[4] = lunches[i];
+                        weeklymenu[4] = lunch;
                     } else if(!weeklymenu[5]) {
-                        weeklymenu [5] = lunches[i];
+                        weeklymenu[5] = lunch;
                     }
-                    lunches.splice(i, 1);
+                    lunchesCopy.splice(i, 1);
+                    i--;
                 } else if(lunch.lunchday == 4){
                     if(!weeklymenu[6]) {
-                        weeklymenu[6] = lunches[i];
+                        weeklymenu[6] = lunch;
                     } else if(!weeklymenu[7]) {
-                        weeklymenu [7] = lunches[i];
+                        weeklymenu[7] = lunch;
                     }
-                    lunches.splice(i, 1);
+                    lunchesCopy.splice(i, 1);
+                    i--;
                 } else if(lunch.lunchday == 5){
                     if(!weeklymenu[8]) {
-                        weeklymenu[8] = lunches[i];
+                        weeklymenu[8] = lunch;
                     } else if(!weeklymenu[9]) {
-                        weeklymenu [9] = lunches[i];
+                        weeklymenu[9] = lunch;
                     }
-                    lunches.splice(i, 1);
+                    lunchesCopy.splice(i, 1);
+                    i--;
                 } else if(lunch.lunchday == 6){
                     if(!weeklymenu[10]) {
-                        weeklymenu[10] = lunches[i];
+                        weeklymenu[10] = lunch;
                     } else if(!weeklymenu[11]) {
-                        weeklymenu [11] = lunches[i];
+                        weeklymenu [11] = lunch;
                     }
-                    lunches.splice(i, 1);
+                    lunchesCopy.splice(i, 1);
+                    i--;
                 } else if(lunch.lunchday == 7){
                     if(!weeklymenu[12]) {
-                        weeklymenu[12] = lunches[i];
+                        weeklymenu[12] = lunch;
                     } else if(!weeklymenu[13]) {
-                        weeklymenu [13] = lunches[i];
+                        weeklymenu[13] = lunch;
                     }
-                    lunches.splice(i, 1);
+                    lunchesCopy.splice(i, 1);
+                    i--;
                 }
                 i++;
             });
 
             //Now weeklymenu is an array with some days filled, these are the "important to be on specific weekdays"-lunches.
-            //Also "lunches" is an array with some holes in it as delete was used.
-            //This is as to not reuse the same lunch twice in a week.
-            //To fix holes in lunches splice is used. If this had been done earlier not all lunches would have been checked for lunchday.
-            /*let loop = 0; //Loopcounter.
-            lunches.forEach(item => {
-                let lunch = Object.values(item);
-
-                //If the lunch is null or undefined.
-                if(!lunch || lunch === undefined || lunch === null) {
-                    //Splice is used at position of "loop" to remove one item and doesn't add any items.
-                    lunches.splice(loop, 1);
-                }
-                //Loop is increased.
-                loop++;
-            });*/
-            console.log("Weeklymenu:");
-            console.log(weeklymenu);
-            console.log("--------------------------");
-            console.log("Lunches:");
-            console.log(lunches);
-            //And now lunches should be an array without holes.
+            //Also "lunchesCopy" is an array of the remaining lunches were lunchday is set to 0.
             //With a for on the weeklymenu we're adding in the remaining lunches.
-            //For-loop is based on 14 instead of weeklymenu.length as the length could be incorrect depending on previously added lunches.
-            //Variable for lunches[j].
+            
+            //Variable for lunchesCopy[j] as this will not match the index of weeklymenu..
             let j = 0;
 
-            for (let i = 0; i < 14; i++) {
+            for (let i = 0; i < weeklymenu.length; i++) {
                 const menuitem = weeklymenu[i];
 
                 //If the item doesn't exist or is undefined.
                 if(!menuitem || menuitem === undefined || menuitem === null){
-                    weeklymenu[i] = lunches[j];
+                    weeklymenu[i] = lunchesCopy[j];
                     j++;
                 }  
             };
 
-            //Now weeklymenu should be an array of 14 items with lunches that should be on specific days on those days.
+            //Now weeklymenu should be an array of 14 items with lunches that are marked as specific days on those days.
             const menu = JSON.stringify(weeklymenu);
 
             //Create query and send to database.
@@ -265,9 +234,8 @@ class Food {
                     //Send error message and return.
                     return err;
                 } else {
-                    //Finally the lunchprio of everything needs to be lowerd so the same lunches aren't repeated the next week.
+                    //Finally the lunchprio of everything needs to be lowerd to 3 so the same lunches aren't repeated the next week.
                     weeklymenu.forEach(item => {
-                        console.log(item);
                         let id = item.item_id;
 
                         connection.query(`UPDATE lunch_item SET lunchprio = 3 WHERE item_id = ?;`, id, (err, result) => {
@@ -275,12 +243,14 @@ class Food {
                             if(err) {
                                 //Send error message and return.
                                 return err;
+                            } else {
+                                //Post successmessage.
+                                console.log(`Lunchitem with id: ${id} was updated with lower priority for comming weeks.`);
                             }
                         });
                     });
-
                     //Show success message.
-                    console.log(`Weekly menu with id: ${weekID} was added.`);
+                    console.log(`Weekly menu with id: ${weekID} was added.`, weeklymenu);
                 }
             });            
         });
